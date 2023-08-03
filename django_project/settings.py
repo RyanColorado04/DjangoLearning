997a3e25f768f117bf6ab864a98855ed15d3a4f5
RyanColorado04 <ryyo1332@colorado.edu>
2023-08-02 19:05:41 -0500

    Made some minor changes

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,8 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
+
+    # 3rd party apps
+    'crispy_forms',
+    'crispy_bootstrap5'
 ]
 
 MIDDLEWARE = [
